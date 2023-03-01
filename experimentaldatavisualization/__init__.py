ade7a59223552205ee9495f1530f2115a5423c10
sd2399325 <devxkong@aliyun.com>
2023-03-01 10:49:51 +0800

    修改Readme内容，并且按照pep8规则进行命名修改。

--- experimentaldatavisualization/__init__.py
+++ experimentaldatavisualization/__init__.py
@@ -3,30 +3,30 @@ import json
 from collections import defaultdict
 
 
-def load_directory_tree(dir_path, lvl=0):
+def load_directory_tree(dir, lvl=0):
     """根据输入的路径及遍历层级加载路径下所有目录树结构
 
-    : param dir_path: 路径
-    : type  dir_path: str
+    : param dir: 路径
+    : type  dir: str
     : param lvl: 遍历层级
     : type  lvl: int
     """
-    dirTree = defaultdict(dict)
-    dirTree['name'] = os.path.basename(dir_path)
-    dirTree['children'] = list()
+    _dir_tree = defaultdict(dict)
+    _dir_tree['name'] = os.path.basename(dir)
+    _dir_tree['children'] = list()
 
     # 迭代遍历所有路径并构成树结构
-    files = [f for f in os.listdir(dir_path)
-             if os.path.isdir(os.path.join(dir_path, f)) and lvl>0]
+    _files = [f for f in os.listdir(dir)
+             if os.path.isdir(os.path.join(dir, f)) and lvl>0]
 
-    for file in files:
+    for file in _files:
         if(lvl == 0):
             break
-        dirTree2 = defaultdict(dict)
-        dirTree2[file] = load_directory_tree(os.path.join(dir_path, file), lvl-1)
+        _dir_tree_child = defaultdict(dict)
+        _dir_tree_child[file] = load_directory_tree(os.path.join(dir, file), lvl-1)
 
-        dirTree['children'].append(dirTree2)
-    return dirTree
+        _dir_tree['children'].append(_dir_tree_child)
+    return _dir_tree
 
 # 测试路径树的结果
 #print(json.dumps(pathTreeJson("D:/projects", 3), indent=4))
