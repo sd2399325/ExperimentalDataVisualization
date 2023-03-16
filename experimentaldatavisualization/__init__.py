b1cee84aaf3ec1c018635024984bb3a0a28cc1e9
sd2399325 <devxkong@aliyun.com>
2023-03-16 15:02:11 +0800

    修改注释

--- experimentaldatavisualization/__init__.py
+++ experimentaldatavisualization/__init__.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 
 
 def load_directory_tree(root_dir, lvl=0):
-    """根据输入的路径及遍历层级加载路径下所有目录树结构
+    """根据输入的路径及遍历层级加载路径下所有子目录及文件，并加载成树形数据结构
+
 
     : param dir: 路径
     : param lvl: 遍历层级
@@ -19,11 +20,10 @@ def load_directory_tree(root_dir, lvl=0):
 
     # 遍历第一层文件内包含的文件夹,具体层数根据传入的遍历层数确定
     for file in files:
-        # if(lvl == 0):
-        #     break
         dir_tree_child = defaultdict(dict)
         dir_tree_child['name'] = file
         dir_tree_child['children'] = load_directory_tree(os.path.join(root_dir, file), lvl-1)
         dir_tree['children'].append(dir_tree_child)
+
     return dir_tree
 
