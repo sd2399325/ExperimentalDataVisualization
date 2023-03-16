5d7f6a488b1b0e085661caaa6fb3afce84c05006
sd2399325 <devxkong@aliyun.com>
2023-03-16 16:03:13 +0800

    修复逻辑bug，增加vscode调试json

--- experimentaldatavisualization/__init__.py
+++ experimentaldatavisualization/__init__.py
@@ -1,4 +1,5 @@
 import os
+import json
 from collections import defaultdict
 
 
@@ -6,7 +7,7 @@ def load_directory_tree(root_dir, lvl=0):
     """根据输入的路径及遍历层级加载路径下所有子目录及文件，并加载成树形数据结构
 
 
-    : param dir: 路径
+    : param root_dir: 路径
     : param lvl: 遍历层级
     : return: 路径树结构
     """
@@ -20,10 +21,12 @@ def load_directory_tree(root_dir, lvl=0):
 
     # 遍历第一层文件内包含的文件夹,具体层数根据传入的遍历层数确定
     for file in files:
+        if(lvl == 0):
+            break
         dir_tree_child = defaultdict(dict)
-        dir_tree_child['name'] = file
-        dir_tree_child['children'] = load_directory_tree(os.path.join(root_dir, file), lvl-1)
-        dir_tree['children'].append(dir_tree_child)
+        dir_tree_child[file] = load_directory_tree(os.path.join(root_dir, file), lvl-1)
 
+        dir_tree['children'].append(dir_tree_child)
     return dir_tree
 
+print(json.dumps(load_directory_tree("D:/projects", ), indent=4))
