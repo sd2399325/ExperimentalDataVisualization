b95ef65e0a541df55b109038dbdcd4d0947140e0
sd2399325 <devxkong@aliyun.com>
2023-03-28 18:49:35 +0800

    修改function 增加模糊匹配逻辑

--- experimentaldatavisualization/__init__.py
+++ experimentaldatavisualization/__init__.py
@@ -12,10 +12,9 @@ def load_directory_tree(root_dir, lvl=0):
     : return: 路径树结构
     """
 
-    # 需要修改 在节点增加节点所在全路径便于查询
-
     dir_tree = defaultdict(dict)
     dir_tree['name'] = os.path.basename(root_dir)
+    dir_tree['dir'] = root_dir
     dir_tree['children'] = list()
 
     # 迭代遍历路径下第一层文件夹并构成树结构
@@ -32,4 +31,29 @@ def load_directory_tree(root_dir, lvl=0):
         dir_tree['children'].append(dir_tree_child)
     return dir_tree
 
-print(json.dumps(load_directory_tree("D:/projects", ), indent=4))
+def filter_directory_tree(dir_tree, key_word):
+    """根据关键词对目录树进行筛选并重新构建目录树
+
+    : param dir_tree: 目录树
+    : param key_word: 关键词
+    : return: 筛选后的目录树
+    """
+    # 递归遍历目录树
+    for key, value in dir_tree.items():
+        if key == 'name':
+            if key_word in value:
+                return dir_tree
+        elif key == 'children':
+            for child in value:
+                for k, v in child.items():
+                    res = filter_directory_tree(v, key_word)
+                    if res:
+                        return res
+        else:
+            continue
+    return None
+
+
+print(json.dumps(load_directory_tree("D:/projects", 2), indent=4))
+
+
